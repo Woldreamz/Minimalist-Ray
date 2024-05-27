ebe41347d6f4b459be82da0c6b5cd3c39093809d
Rainer Nsa <woldreamz@gmail.com>
2024-05-27 21:57:38 +0100

    City of God

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" type="image/png" href="https://i.postimg.cc/xjwDWjgB/woldreamz-inc-favicon-black.png" />
       <body className={inter.className}>{children}</body>
     </html>
   );
