06274e560d9018dc90e11836604d6509eb9ff3cb
Rainer Nsa <woldreamz@gmail.com>
2024-06-16 20:46:57 +0100

    City of God

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -32,7 +32,7 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
             <h2 className="uppercase tracking-widest text-xs text-blue-100 max-w-80">
-                Dynamc Web Magic with Next.js
+                Dynamic Web Magic with Woldreamz Inc
             </h2>
 
             {/**
@@ -42,7 +42,7 @@ const Hero = () => {
            */}
             <TextGenerateEffect         
                 className="text-center text-[40px] md:text-5xl lg:text-6xl"
-                words="Transforming Concepts into Seamless User Experiences"
+                words="Digivolving Concepts into Seamless User Experiences"
             />
 
             <p className="text-center md:tracking-wider mb-4 text-xs md:text-lg lg:text-2xl">
